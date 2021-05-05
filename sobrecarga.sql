3e3b164147fe0f47a413a65754688bca2f47bd39
JUANITOTELO <jdmm.jdmm31@gmail.com>
2021-05-05 16:54:52 -0500

    Formato de la fecha dmy

--- sobrecarga.sql
+++ sobrecarga.sql
@@ -1,8 +1,7 @@
-Copy autor from 'C:\Sobrecarga Postgres\autor.csv' delimiter ',' CSV HEADER;
-Copy editorial from 'C:\Sobrecarga Postgres\editoriales.csv' delimiter ',' CSV HEADER;
-Copy idioma from 'C:\Sobrecarga Postgres\idioma.csv' delimiter ',' CSV HEADER;
-Copy libro from 'C:\Sobrecarga Postgres\libross.csv' delimiter ',' CSV HEADER;
-Copy autor_libro from 'C:\Sobrecarga Postgres\libro_autor.csv' delimiter ',' CSV HEADER;
-
-delete from libro
-
+--Agregar direccion de las tablas
+set datestyle = mdy;
+Copy autor from 'autor.csv' delimiter ',' encoding 'UTF-8' CSV HEADER;
+Copy editorial from 'editoriales.csv' delimiter ',' encoding 'UTF-8' CSV HEADER;
+Copy idioma from 'idioma.csv' delimiter ',' encoding 'UTF-8' CSV HEADER;
+Copy libro from 'libross.csv' delimiter ',' encoding 'UTF-8' CSV HEADER;
+Copy autor_libro from 'libro_autor.csv' delimiter ',' encoding 'UTF-8' CSV HEADER;
