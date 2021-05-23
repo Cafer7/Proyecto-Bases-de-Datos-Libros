76c510aaf6913ad4228304d533715be07d2d0f99
JUANITOTELO <jdmm.jdmm31@gmail.com>
2021-05-23 01:58:19 -0500

    O.o

--- app/data.py
+++ app/data.py
@@ -56,4 +56,16 @@ app.layout = html.Div(children=[
     Input("input_search", "value"),
 )
 
-
+def cb_render(*vals):
+    if vals[0] != None and vals[0] != "":
+        busquedas = []
+        con.openConnection()
+        busqueda = pd.read_sql_query(buscar_libro_por_titulo(vals[0].lower()), con.connection)
+        con.closeConnection()
+        df_busqueda = pd.DataFrame(busqueda, columns=["titulo"])
+        table = dbc.Table.from_dataframe(df_busqueda, striped=True, bordered=True, hover=False)
+        busquedas = df_busqueda["titulo"].astype(str).values.tolist()
+        return table
+        # return html.Ul(children=[html.Li(i) for i in busquedas])
+    else:
+        return [""]
