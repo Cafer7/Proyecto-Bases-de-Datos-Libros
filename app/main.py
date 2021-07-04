b338b16290d746be3fce576bc987546d80bcede2
JUANITOTELO <jdmm.jdmm31@gmail.com>
2021-07-04 18:38:26 -0500

    Tablas con id

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 from data import *
 if __name__ == "__main__":
-    app.run_server(debug=True)
-    # app.run_server(port=8050,host='0.0.0.0')
+    # app.run_server(debug=True)
+    app.run_server(port=8050,host='0.0.0.0')
